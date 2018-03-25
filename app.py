1f94ff15493e05aca768146a2c5f3228d3ec3d71
hasika12 <saii2882@gmail.com>
2018-03-25 13:56:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name = str(name) + "Love to code"
+	new_name = str(name) + " Loves to code for you."
 	return render_template('index.html', name=new_name)
 
 
